af385fa26f01ba75be7cc834a49815c69995e055
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-07-05 19:56:36 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createRequire } from 'module'; 
-import { Buffer } from 'buffer';
+
 
 const require = createRequire(import.meta.url); 
 
 export default defineConfig({
+  plugins: [react()],
   define: {
-    'global': {},
-    'process.env': {},
-    'Buffer': JSON.stringify(Buffer)
-
+    'global.Buffer': 'buffer.Buffer'
   },
-   optimizeDeps: {
+  optimizeDeps: {
     include: ['buffer']
   },
-  plugins: [react()],
+  resolve: {
+    alias: {
+      buffer: 'buffer'
+    }
+  }
 });
+

c3b08446ccbb720de5f6b67497113e6b0067e99c
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-07-22 15:00:31 +0530

    Update Home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -32,6 +32,8 @@ const Home = () => {
   useEffect(() => {
     if (!isAuthenticated) return;
     dispatch(initializeSocket(userProfile?._id));
+    // Ensure conversations are fetched after login
+    dispatch(require("../../store/slice/message/message.thunk").getConversationsThunk());
   }, [isAuthenticated]);
 
   useEffect(() => {

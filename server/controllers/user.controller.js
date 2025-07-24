9c4bf246dfb13cb8f616c1ab45a9e0fcb06cf395
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-07-24 19:35:48 +0530

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -46,8 +46,8 @@ export const register = asyncHandler(async (req, res, next) => {
     .cookie("token", token, {
       expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: "None",
+      secure: process.env.NODE_ENV === 'production' ? true : false,
+      sameSite: process.env.NODE_ENV === 'production' ? "None" : "Lax",
     })
     .json({
       success: true,

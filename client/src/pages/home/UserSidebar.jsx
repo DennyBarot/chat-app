cb3062d15faddfa76cf02d3af6f97ec393f6365b
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-08-04 20:44:41 +0530

    Revert "theme"
    This reverts commit c8b7b280d362fb467c0a74bf1d0ad7007d13399f.

--- client/src/pages/home/UserSidebar.jsx
+++ client/src/pages/home/UserSidebar.jsx
@@ -7,7 +7,6 @@ import { logoutUserThunk } from "../../store/slice/user/user.thunk";
 import { getConversationsThunk } from "../../store/slice/message/message.thunk";
 import { useSocket } from "../../context/SocketContext";
 import { setSelectedUser } from "../../store/slice/user/user.slice";
-import { useTheme } from "../../context/ThemeContext";
 
 const UserSidebar = ({ onUserSelect }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -19,7 +18,6 @@ const UserSidebar = ({ onUserSelect }) => {
   const { userProfile } = useSelector((state) => state.userReducer);
   const conversations = useSelector((state) => state.messageReducer.conversations);
   const [users, setUsers] = useState([]);
-  const { theme, toggleTheme } = useTheme();
 
   const socket = useSocket();
 
@@ -151,18 +149,14 @@ const UserSidebar = ({ onUserSelect }) => {
   return (
     <div className="w-80 h-full flex flex-col bg-white shadow-lg z-10">
       {/* Header */}
-      <div className="p-4 border-b border-slate-200 flex justify-between items-center">
+      <div className="p-4 border-b border-slate-200">
         <h1 className="text-2xl font-bold text-indigo-700 flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />
           </svg>
           CHAT APP
         </h1>
-        <label className="swap swap-rotate">
-          <input type="checkbox" onChange={toggleTheme} checked={theme !== 'Default'} />
-          <svg className="swap-on fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
-          <svg className="swap-off fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13.14a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13.14Zm-5.09-1.54A10.73,10.73,0,0,1,14.08,5.1,8.4,8.4,0,0,1,14.8,2.92,8.07,8.07,0,0,1,18,11.2a8.51,8.51,0,0,1-1.5-1.61Z"/></svg>
-        </label>
+        
       </div>
 
       {/* Search */}

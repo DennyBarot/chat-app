9c9b7c2af3b416fe4abe55a6f937ed234fcb3a9f
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-08-04 20:08:14 +0530

    Revert "Update UserSidebar.jsx"
    This reverts commit 06f6cdf90291d6159408e4d5116cb3de3edfeb9f.

--- client/src/pages/home/UserSidebar.jsx
+++ client/src/pages/home/UserSidebar.jsx
@@ -151,7 +151,7 @@ const UserSidebar = ({ onUserSelect }) => {
   return (
     <div className="w-80 h-full flex flex-col bg-white shadow-lg z-10">
       {/* Header */}
-      <div className="p-4 border-b border-slate-200 flex items-center justify-between">
+      <div className="p-4 border-b border-slate-200">
         <h1 className="text-2xl font-bold text-indigo-700 flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />

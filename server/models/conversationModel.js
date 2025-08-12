0e77142c4aacf53a77b7a56cb2575ddb260a91c7
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-08-12 21:10:50 +0530

    Update conversationModel.js

--- server/models/conversationModel.js
+++ server/models/conversationModel.js
@@ -16,7 +16,6 @@ const conversationSchema = new mongoose.Schema(
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true,
-        index: true,
       },
     ],
     /**

70bb81be216b287271943fa305c58e5f8584b528
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-08-12 21:33:02 +0530

    Revert "Update conversationModel.js"
    This reverts commit 0e77142c4aacf53a77b7a56cb2575ddb260a91c7.

--- server/models/conversationModel.js
+++ server/models/conversationModel.js
@@ -16,6 +16,7 @@ const conversationSchema = new mongoose.Schema(
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true,
+        index: true,
       },
     ],
     /**

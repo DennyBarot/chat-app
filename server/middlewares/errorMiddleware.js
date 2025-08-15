79f5d668b89b572a150c56c87ce01df57ee79e35
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-08-15 23:54:14 +0530

    Update errorMiddleware.js

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -1,4 +1,4 @@
-export const errorMiddleware = (err, req, res) => {
+export const errorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 

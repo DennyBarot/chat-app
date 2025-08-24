7e352435ad719f1228f5162724efe2cb0cfc2706
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-08-24 13:51:34 +0530

    0.1

--- server/routes/conversation.routes.js
+++ server/routes/conversation.routes.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import { createConversation, getConversations, getConversationById, updateConversation, deleteConversation } from '../controllers/conversation.controller.js';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { isAuthenticated } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 // Create a new conversation
-router.post('/', authMiddleware, createConversation);
+router.post('/', isAuthenticated, createConversation);
 
 // Get all conversations for a user
-router.get('/:userId', authMiddleware, getConversations);
+router.get('/:userId', isAuthenticated, getConversations);
 
 // Get conversation by id
-router.get('/id/:id', authMiddleware, getConversationById);
+router.get('/id/:id', isAuthenticated, getConversationById);
 
 // Update a conversation
-router.put('/:id', authMiddleware, updateConversation);
+router.put('/:id', isAuthenticated, updateConversation);
 
 // Delete a conversation
-router.delete('/:id', authMiddleware, deleteConversation);
+router.delete('/:id', isAuthenticated, deleteConversation);
 
 export default router;

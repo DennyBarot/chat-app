6e50d81bd54947f0af1c21c7b3e0e7dc59d4b97d
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-08-29 22:06:58 +0530

    1

--- client/src/pages/home/User.jsx
+++ client/src/pages/home/User.jsx
@@ -64,13 +64,7 @@ const User = ({ userDetails, showUnreadCount = true, isTyping = false, displayTy
             </svg>
           </span>
         )}
-        {isUserInCall && (
-          <span className="absolute bottom-0 right-0 w-3.5 h-3.5 bg-blue-500 border-2 border-background rounded-full flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-2 w-2 text-white" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-            </svg>
-          </span>
-        )}
+       
       </div>
       
       <div className="flex-1 min-w-0">
@@ -88,8 +82,6 @@ const User = ({ userDetails, showUnreadCount = true, isTyping = false, displayTy
             <p className="text-sm text-primary animate-pulse">typing...</p>
           ) : isUserRecording ? (
             <p className="text-sm text-primary animate-pulse">Voice recording...</p>
-          ) : isUserInCall ? (
-            <p className="text-sm text-primary animate-pulse">In a call...</p>
           ) : displayType === 'sidebar' ? (
             userDetails?.lastMessage ? (
               <p className="text-sm text-text-secondary truncate">

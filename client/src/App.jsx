7a1483e90f6e32472855d8ec34e240898dbbd6e8
Hiren Brahmbhatt <hirenbrahmbhatt411@gmail.com>
2025-08-30 13:58:15 +0530

    7.3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   const dispatch = useDispatch();
   const socket = useSocket();
   const { userProfile } = useSelector((state) => state.userReducer);
-  const { call, callAccepted, callEnded, stream, caller, callerSignal } = useSelector((state) => state.callReducer || {});
+  const { call, callAccepted, callEnded, stream, caller, callerSignal, isStreamReady } = useSelector((state) => state.callReducer || {});
 
   const myVideo = useRef();
   const userVideo = useRef();
